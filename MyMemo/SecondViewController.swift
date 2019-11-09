c6d153e2df0dc184a5e3c4bc6ebb88b23b6cfe19
Cistudent <cistudent@CISs-Mac.local>
2019-11-09 02:34:15 -0800

    Initial Commit

---
+++ MyMemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyMemo
+//
+//  Created by Cistudent on 11/9/19.
+//  Copyright © 2019 Cistudent. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
